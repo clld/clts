2cc006522b4e906837b339878a94149479f666f5
lingulist <mattis.list@lingpy.org>
2018-01-08 15:24:50 +0100

    updating models to accound for new schema

--- clts/models.py
+++ clts/models.py
@@ -16,35 +16,31 @@ from clld import interfaces
 from clld.db.meta import Base, CustomModelMixin
 from clld.db.models.common import Language, Value, Parameter, Contribution
 
-
-#-----------------------------------------------------------------------------
-# specialized common mapper classes
-#-----------------------------------------------------------------------------
-#@implementer(interfaces.ILanguage)
-#class cltsLanguage(CustomModelMixin, Language):
-#    pk = Column(Integer, ForeignKey('language.pk'), primary_key=True)
-
 @implementer(interfaces.IParameter)
 class SoundSegment(CustomModelMixin, Parameter):
     pk = Column(Integer, ForeignKey('parameter.pk'), primary_key=True)
     grapheme = Column(Unicode)
     aliases = Column(Unicode)
     representation = Column(Integer)
-    transcription_systems = Column(Unicode)
-    transcription_data = Column(Unicode)
-    sound_classes = Column(Unicode)
+    unicode = Column(Unicode)
+    generated = Column(Boolean)
+    reflexes = Column(Unicode)
 
 @implementer(interfaces.IValue)
 class Grapheme(CustomModelMixin, Value):
     pk = Column(Integer, ForeignKey('value.pk'), primary_key=True)
     grapheme = Column(Unicode)
-    transcription_data = Column(Unicode)
     url = Column(Unicode)
     features = Column(Unicode)
     image = Column(Unicode)
+    datatype = Column(Unicode)
+    bipa_grapheme = Column(Unicode)
+    dataset = Column(Unicode)
 
 @implementer(interfaces.IContribution)
-class TranscriptionData(CustomModelMixin, Contribution):
+class CLTSDataSet(CustomModelMixin, Contribution):
     pk = Column(Integer, ForeignKey('contribution.pk'), primary_key=True)
     items = Column(Integer)
+    datatype = Column(Unicode)
+
     
