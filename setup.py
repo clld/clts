5ed8720e5e5305f0005ebbd50af7aa3527fa743e
Robert Forkel <xrotwang@googlemail.com>
2022-07-20 09:07:41 +0200

    upgrade clld

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        "clld>=9.2.1",
         "clldmpg>=4.2",
-        "clld>=8",
         "clldutils>=3.5",
         'markdown',
         'tqdm',
